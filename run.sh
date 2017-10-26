e8dabb0b85aa0ecec0dedba4baca6689d3be3969
ben0 <ben0@users.noreply.github.com>
2017-10-26 09:26:46 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+# Update/Upgrade Kali
+apt-get update && apt-get dist-upgrade -y
+apt-get install open-vm-tools open-vm-tools-desktop cron-apt doublecmd-gtk terminator -y
+
+# Nmap/Metasploit
+nmap --script-updatedb
+msfupdate
+service postgresql start
+msfdb init
+update-rc.d postgresql enable
