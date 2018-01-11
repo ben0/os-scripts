38259f95f8de5a9111e149bafdb47cdc6be12144
ben0 <ben0@users.noreply.github.com>
2018-01-11 14:29:35 +0000

    Update kali-rolling.sh

--- kali-rolling.sh
+++ kali-rolling.sh
@@ -3646,6 +3646,11 @@ git clone -q -b master https://github.com/longld/peda.git ~/peda \
 echo "source ~/peda/peda.py" >> ~/.gdbinit
 git pull -q
 
+##### Dependancies for hydra
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}hydra deps${RESET} (APT)"
+apt -y -qq install libssl-dev libgcrypt-dev libidb2-0-dev libcurses-ocmal-dev libpcre2-dev libpq-dev default-mysqlclient-dev libssh2-1-dev \
+  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+
 ##### Install THC Hydra - DEVEL (GIT)
 (( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}hydra${RESET} (GIT)"
 apt -y -qq install git \
